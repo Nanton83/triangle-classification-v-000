b2eedfe4b1bd8c0ac3e68e174991cbfe4f80f955
Nick Romans <nicholas.romans@gmail.com>
2018-10-01 23:29:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,13 @@ class Triangle
     
   end
   
-  def real_triangle1
-    side1.positive 
+  def im_a_real_triangle1
+    side1.positive? && side2.positive? && side3.positive?
+  end
+  
+  def im_a_real_triangle2
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
   
     class TriangleError < StandardError
       
@@ -22,39 +27,5 @@ class Triangle
 end
 
 
-  class Triangle
-  attr_accessor :x, :y, :z
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
-  end
 
-  def kind
-    if kosher_triangle == true && kosher_triangle2 == true 
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
-    if x == y && y == z && x == z
-      :equilateral
-    elsif x != y && y != z && z != x
-      :scalene 
-    else 
-      :isosceles
-    end 
-  end
   
-  def kosher_triangle 
-    x.positive? && y.positive? && z.positive?
-  end 
-  
-  def kosher_triangle2 
-    x + y > z && x + z > y && y + z > x
-  end 
-end
-
-class TriangleError < StandardError
-  
-end
