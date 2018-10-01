d2a399c1d7594ce907b095dd680b38d59df3042d
Nick Romans <nicholas.romans@gmail.com>
2018-10-01 23:32:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
   end
   
   def kind
+    if im_a_real_triangle1 && im_a_real_triangle2 == true 
+      
     
   end
   
@@ -27,5 +29,41 @@ class Triangle
 end
 
 
+class Triangle
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+  
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
+end
+
+class TriangleError < StandardError
+  
+end
 
   
