557867dbe78cfcba220e60c2a70cd7db2faddf07
Nick Romans <nicholas.romans@gmail.com>
2018-10-02 00:23:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,12 +14,12 @@ class Triangle
       !(@side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1)
       raise TriangleError
       else
-      if (@side1 == @side2) && (@side2 == @side3)
-        :equilateral
-      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
-        :isosceles
-      elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
-        :scalene
+        if (@side1 == @side2) && (@side2 == @side3)
+          :equilateral
+        elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+          :isosceles
+        elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
+          :scalene
       end
     end
   end
