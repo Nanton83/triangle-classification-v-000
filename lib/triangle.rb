91bb9f05eaf5993512eadf750847ce06aadeac43
Nick Romans <nicholas.romans@gmail.com>
2018-10-02 00:07:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,14 @@ class Triangle
       elsif
       im_a_real_triangle2 == false
       raise TriangleError
-      
+      else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
   end 
   
